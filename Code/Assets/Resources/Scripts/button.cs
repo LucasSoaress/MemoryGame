893c89035231dc7338a7803d9ddd6fb7d82719b6
Lucas Soares <lsoares267@gmail.com>
2016-09-23 09:25:26 -0300

    Memory Game
    Finish

--- Code/Assets/Resources/Scripts/button.cs
+++ Code/Assets/Resources/Scripts/button.cs
@@ -4,30 +4,19 @@ using System.Collections;
 
 public class button : MonoBehaviour
 {
-    public  bool clicado;
     public  Sprite frenteCarta;
     public Sprite versoCarta;
 
-	void Update ()
-    {
-	    if (clicado)
-        {
-            //this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(0f, -180f, 0f), 0.1f);
-
-           // if (this.transform.eulerAngles.y >= 90f)
-            //{
-                this.GetComponent<Image>().sprite = frenteCarta;
-            //}
-        }
-
-	}
-
     public void click()
     {
-        if (detectar.pares.Length == 0 || detectar.pares.Length == 1)
+        if (this.gameObject.tag == "Player" || this.gameObject.tag == "Cards")
         {
-            clicado = true;
-            this.gameObject.tag = "virado";
-        }  
+            if (detectar.pares.Length == 0 || detectar.pares.Length == 1)
+            {
+                this.GetComponent<Image>().sprite = frenteCarta;
+                this.gameObject.tag = "virado";
+            } 
+        }
+         
     }
 }
--- Code/Assets/Resources/Scripts/detectar.cs
+++ Code/Assets/Resources/Scripts/detectar.cs
@@ -7,48 +7,94 @@ public class detectar : MonoBehaviour
     public static GameObject[] pares;
     private float tempoVirada;
     public Sprite verso;
-   public static button b = new button();
+    public static button b = new button();
+    private string nomeCartaUm;
+    private string nomeCartaDois;
+
+    public Image explicacao;
+    public Sprite[] linguagemExplicacao;
+    public Image botaoFechar;
 
     void Start ()
     {
         tempoVirada = 0;
+        explicacao = explicacao.GetComponent<Image>();
+        explicacao.enabled = false;
+        botaoFechar.enabled = false;
     }
 	
 	void Update ()
     {
         pares = GameObject.FindGameObjectsWithTag("virado");
         
-
         if (pares.Length == 2)
         {
+            nomeCartaDois = pares[1].GetComponent<Image>().sprite.name;
+            nomeCartaUm = pares[0].GetComponent<Image>().sprite.name;
+
             tempoVirada += Time.deltaTime;
-            if (pares[0].GetComponent<Image>().sprite.name == pares[1].GetComponent<Image>().sprite.name)
+
+            if (nomeCartaUm == nomeCartaDois)
             {
-                Debug.Log("acertei o par");
-                trocarTag();
+                switch(nomeCartaUm)
+                {
+                    case "c#":
+                        aparecerExplicacao(0);
+                        break;
+                    case "c":
+                        aparecerExplicacao(1);
+                        break;
+                    case "java":
+                        aparecerExplicacao(2);
+                        break;
+                    case"javascript":
+                        aparecerExplicacao(3);
+                        break;
+                    case"pascal":
+                        aparecerExplicacao(4);
+                        break;
+                    case"python":
+                        aparecerExplicacao(5);
+                        break;
+                    case"php":
+                        aparecerExplicacao(6);
+                        break;
+                    case"ruby":
+                        aparecerExplicacao(7);
+                        break;
+                    case"lua":
+                        aparecerExplicacao(8);
+                        break;
+                }
+                trocarTag("Acertou");
             }
 
             else if (tempoVirada >= 1)
             {
                 pares[0].GetComponent<Image>().sprite = verso;
                 pares[1].GetComponent<Image>().sprite = verso;
-                b.clicado = false;
-                Debug.Log("errei");
-              //  pares[0].transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(0f, 0f, 0f), 0.1f);
-              //  pares[1].transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(0f, 0f, 0f), 0.1f);
-                // if (pares[0].transform.eulerAngles.y >= 90)
-                // {
-                     
-                //}
-                trocarTag();
+                trocarTag("Player");
                 tempoVirada = 0;
             }
         }
     }
 
-    void trocarTag()
+    void trocarTag(string tag)
     {
-        pares[0].tag = "Player";
-        pares[1].tag = "Player";
+        pares[0].tag = tag;
+        pares[1].tag = tag;
+    }
+
+    void aparecerExplicacao(int position)
+    {
+        explicacao.enabled = true;
+        explicacao.GetComponent<Image>().sprite = linguagemExplicacao[position];
+        botaoFechar.enabled = true;
+    }
+
+    public void fecharExplicacao()
+    {
+        explicacao.enabled = false;
+        botaoFechar.enabled = false;
     }
 }
